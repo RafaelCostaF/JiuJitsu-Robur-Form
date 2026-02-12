e93c775dd7b5276424be192589d590a75b2092c9
RafaelCostaF <rafacsasuke@gmail.com>
2026-02-11 22:35:03 -0300

    ajuste layout e metadados

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,35 @@
 import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Robur Jiu Jitsu - Formulario",
   description: "Formulário de cadastro - Robur Jiu Jitsu",
 };
 
+
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
